f1c9667d16a65b50c9f85f970625225bf7bb58a2
epns152 <pavlenko.roman@lll.kpi.ua>
2023-12-21 19:13:34 +0200

    Create interfaces for all repositories

---
+++ src/main/java/org/example/app/dal/repositories/interfaces/ComplaintRepository.java
@@ -0,0 +1,8 @@
+package org.example.app.dal.repositories.interfaces;
+
+import org.example.app.dal.entities.Complaint;
+
+import java.util.UUID;
+
+public interface ComplaintRepository extends Repository<Complaint, UUID> {
+}
---
+++ src/main/java/org/example/app/dal/repositories/interfaces/ProtocolRepository.java
@@ -0,0 +1,8 @@
+package org.example.app.dal.repositories.interfaces;
+
+import org.example.app.dal.entities.Protocol;
+
+import java.util.UUID;
+
+public interface ProtocolRepository extends Repository<Protocol, UUID> {
+}
